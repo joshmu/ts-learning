ed7e285afd3611a24a5d62894c9117f083e7900f
Josh Mu <mu@joshmu.com>
2020-07-26 17:12:19 +1000

    :zap: validation function with interface

--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+function validate(validataleInput) {
+    var isValid = true;
+    if (validataleInput.required) {
+        isValid = isValid && validataleInput.value.toString().trim().length !== 0;
+    }
+    if (validataleInput.minLength != null &&
+        typeof validataleInput.value === 'string') {
+        isValid =
+            isValid && validataleInput.value.length >= validataleInput.minLength;
+    }
+    if (validataleInput.maxLength != null &&
+        typeof validataleInput.value === 'string') {
+        isValid =
+            isValid && validataleInput.value.length <= validataleInput.maxLength;
+    }
+    if (validataleInput.min != null &&
+        typeof validataleInput.value === 'number') {
+        isValid = isValid && validataleInput.value >= validataleInput.min;
+    }
+    if (validataleInput.max != null &&
+        typeof validataleInput.value === 'number') {
+        isValid = isValid && validataleInput.value <= validataleInput.max;
+    }
+    return isValid;
+}
 // autobind decorator
 function autobind(target, methodName, descriptor) {
     var originalMethod = descriptor.value;
@@ -31,9 +56,48 @@ var ProjectInput = /** @class */ (function () {
         this.configure();
         this.attach();
     }
+    ProjectInput.prototype.gatherUserInput = function () {
+        var title = this.titleInputElement.value;
+        var description = this.descriptionInputElement.value;
+        var people = this.peopleInputElement.value;
+        var titleValidatable = {
+            value: title,
+            required: true,
+        };
+        var descValidatable = {
+            value: description,
+            required: true,
+            minLength: 5,
+        };
+        var peopleValidatable = {
+            value: +people,
+            required: true,
+            min: 1,
+            max: 5,
+        };
+        if (!validate(titleValidatable) ||
+            !validate(descValidatable) ||
+            !validate(peopleValidatable)) {
+            alert('Invalid input please try again.');
+            return;
+        }
+        else {
+            return [title, description, +people];
+        }
+    };
+    ProjectInput.prototype.clearInputs = function () {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    };
     ProjectInput.prototype.submitHandler = function (event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        var userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            var title = userInput[0], desc = userInput[1], people = userInput[2];
+            console.log({ userInput: userInput });
+            this.clearInputs();
+        }
     };
     ProjectInput.prototype.configure = function () {
         this.element.addEventListener('submit', this.submitHandler);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,48 @@
+// validator
+interface Validatable {
+  value: string | number
+  required?: boolean
+  minLength?: number
+  maxLength?: number
+  min?: number
+  max?: number
+}
+
+function validate(validataleInput: Validatable): boolean {
+  let isValid = true
+  if (validataleInput.required) {
+    isValid = isValid && validataleInput.value.toString().trim().length !== 0
+  }
+  if (
+    validataleInput.minLength != null &&
+    typeof validataleInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validataleInput.value.length >= validataleInput.minLength
+  }
+  if (
+    validataleInput.maxLength != null &&
+    typeof validataleInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validataleInput.value.length <= validataleInput.maxLength
+  }
+  if (
+    validataleInput.min != null &&
+    typeof validataleInput.value === 'number'
+  ) {
+    isValid = isValid && validataleInput.value >= validataleInput.min
+  }
+  if (
+    validataleInput.max != null &&
+    typeof validataleInput.value === 'number'
+  ) {
+    isValid = isValid && validataleInput.value <= validataleInput.max
+  }
+
+  return isValid
+}
+
 // autobind decorator
 function autobind(
   target: any,
@@ -48,10 +93,54 @@ class ProjectInput {
     this.attach()
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const title = this.titleInputElement.value
+    const description = this.descriptionInputElement.value
+    const people = this.peopleInputElement.value
+
+    const titleValidatable: Validatable = {
+      value: title,
+      required: true,
+    }
+    const descValidatable: Validatable = {
+      value: description,
+      required: true,
+      minLength: 5,
+    }
+    const peopleValidatable: Validatable = {
+      value: +people,
+      required: true,
+      min: 1,
+      max: 5,
+    }
+
+    if (
+      !validate(titleValidatable) ||
+      !validate(descValidatable) ||
+      !validate(peopleValidatable)
+    ) {
+      alert('Invalid input please try again.')
+      return
+    } else {
+      return [title, description, +people]
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = ''
+    this.descriptionInputElement.value = ''
+    this.peopleInputElement.value = ''
+  }
+
   @autobind
   private submitHandler(event: Event) {
     event.preventDefault()
-    console.log(this.titleInputElement.value)
+    const userInput = this.gatherUserInput()
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput
+      console.log({ userInput })
+      this.clearInputs()
+    }
   }
 
   private configure() {
