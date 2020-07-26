090bd823389a536f970194c5c4a62f414e1ac395
Josh Mu <mu@joshmu.com>
2020-07-26 15:20:54 +1000

    :sparkles: form rendered through constructor

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,16 @@
 "use strict";
-const hello = 'hello';
-const say = (msg) => console.log(msg);
-say(hello);
+var ProjectInput = /** @class */ (function () {
+    function ProjectInput() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        var importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    ProjectInput.prototype.attach = function () {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    };
+    return ProjectInput;
+}());
+var prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
